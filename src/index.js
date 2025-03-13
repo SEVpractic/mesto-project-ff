312292faef1bef075b62245efd9ea365ab9ba9ca
Sokolov <wisebrain@yandex.ru>
2025-03-13 19:39:22 +0300

    fix: исправлена ошибка изза которой проект не собирался

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './pages/index.css'
+
+import { initialCards } from './scripts/cards'
+
+const templateContent = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(card => {
+  placesList.append(createCard(card, removeCard))
+});
+
+function createCard(cardData = {}, removeCard) {
+  const card = templateContent.querySelector('.card').cloneNode(true);
+  const cardImg = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+  const cardRemoveBtn = card.querySelector('.card__delete-button');
+
+  cardImg.src = cardData.link ? cardData.link : '';
+  cardTitle.textContent = cardData.name ? cardData.name : '';
+
+  cardRemoveBtn.addEventListener('click', () => removeCard(card));
+
+  return card;
+}
+
+function removeCard(card) {
+  if (!card) return;
+  card.remove();
+}
